5e2a77d4f459cb63aafb765cd9a0b08d61e5a7b3
Sekhar Karedla <sekhar.karedla@gmail.com>
2018-12-25 16:36:46 +0530

    decoding JWT

--- src/github.com/sekharkaredla/dswac/CreateJwt/CreateJWT_Encode_and_Decode.go
+++ src/github.com/sekharkaredla/dswac/CreateJwt/CreateJWT_Encode_and_Decode.go
@@ -14,8 +14,19 @@ func main() {
 	claims := token.Claims.(jwt.MapClaims)
 
 	claims["ownerHash"] = "test_owner_hash"
-	claims["filename"] = "filename"
+	claims["fileName"] = "filename"
 	claims["fileHash"] = "file_hash"
 	tokenString, _ := token.SignedString(mySiginingKey)
 	fmt.Println(tokenString)
+
+	claims = jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil {
+		fmt.Println("Error is getting claims ", err)
+	}
+	for key, val := range claims {
+		fmt.Printf("Key: %v, value: %v\n", key, val)
+	}
 }
