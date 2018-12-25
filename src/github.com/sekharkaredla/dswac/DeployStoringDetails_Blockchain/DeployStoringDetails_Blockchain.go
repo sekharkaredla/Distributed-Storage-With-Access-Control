319700d3ae07a6f13123f973e282931563d2f3aa
Sekhar Karedla <sekhar.karedla@gmail.com>
2018-12-25 19:17:12 +0530

    Get and Set JWT working

--- src/github.com/sekharkaredla/dswac/DeployStoringDetails_Blockchain/DeployStoringDetails_Blockchain.go
+++ src/github.com/sekharkaredla/dswac/DeployStoringDetails_Blockchain/DeployStoringDetails_Blockchain.go
@@ -20,22 +20,24 @@ func main() {
 	address, _, contract, err := DeployStoringDetails(
 		auth,
 		blockchain,
-		[]byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaWxlSGFzaCI6ImZpbGVfaGFzaCIsImZpbGVOYW1lIjoiZmlsZW5hbWUiLCJvd25lckhhc2giOiJ0ZXN0X293bmVyX2hhc2gifQ.qbNU91JBZy2dXOK5vjsWVDxARILQ1CVyONJLxB5Vl7I"),
+		//		[]byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmaWxlSGFzaCI6ImZpbGVfaGFzaCIsImZpbGVOYW1lIjoiZmlsZW5hbWUiLCJvd25lckhhc2giOiJ0ZXN0X293bmVyX2hhc2gifQ.qbNU91JBZy2dXOK5vjsWVDxARILQ1CVyONJLxB5Vl7I"),
+		[]byte("old bytes"),
 	)
-
-	blockchain.Commit()
-	fmt.Println(blockchain)
-
 	if err != nil {
 		fmt.Println("Failed to deploy the Inbox contract: %v", err)
 	}
-
 	if len(address.Bytes()) == 0 {
 		fmt.Println("Expected a valid deployment address. Received empty address byte array instead")
 	}
 	out := fmt.Sprintf("address : %s , contract : %s", address, contract)
 	fmt.Println(out)
+	blockchain.Commit()
 	if got, err := contract.GetJWT(nil); err == nil {
-		fmt.Println("Expected message to be: Temp JWT. Got : ", string(got))
+		fmt.Println("current JWT : ", string(got))
+	}
+	_, err = contract.SetJWT(auth, []byte("new bytes"))
+	blockchain.Commit()
+	if got, err := contract.GetJWT(nil); err == nil {
+		fmt.Println("new JWT : ", string(got))
 	}
 }
