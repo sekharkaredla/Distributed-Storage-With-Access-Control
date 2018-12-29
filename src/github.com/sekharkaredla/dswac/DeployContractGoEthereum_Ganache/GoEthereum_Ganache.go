751e7265fbeaebbed1673715aef22a5f18460893
Sekhar Karedla <sekhar.karedla@gmail.com>
2018-12-29 13:47:24 +0530

    Ganache and Go-Eth tried

---
+++ src/github.com/sekharkaredla/dswac/DeployContractGoEthereum_Ganache/GoEthereum_Ganache.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	big "math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+
+	//		"github.com/ethereum/go-ethereum/accounts/abi/bind/backends"
+	//	"github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	web3 "github.com/regcostajr/go-web3"
+	"github.com/regcostajr/go-web3/complex/types"
+	dto "github.com/regcostajr/go-web3/dto"
+	providers "github.com/regcostajr/go-web3/providers"
+)
+
+const GanacheURL = "127.0.0.1:8545"
+
+func DeployContract() (string, error) {
+	abi := StoringDetailsABI
+	binary := StoringDetailsBin
+	var connection = web3.NewWeb3(providers.NewHTTPProvider(GanacheURL, 10, false))
+	contract, err := connection.Eth.NewContract(abi)
+	if err != nil {
+		fmt.Errorf("error generating contract with ABI")
+	}
+	transaction := new(dto.TransactionParameters)
+	Accounts, err := connection.Eth.ListAccounts()
+	if err != nil {
+		fmt.Errorf("error getting the accounts")
+	}
+	coinbase, err := connection.Eth.GetCoinbase()
+	fmt.Println("Accounts : ", Accounts)
+	transaction.From = coinbase
+	transaction.To = Accounts[3]
+	transaction.From = Accounts[0]
+	transaction.Data = types.ComplexString(binary)
+	transaction.Gas = big.NewInt(900000)
+	fmt.Println(transaction)
+	hash, err := contract.Deploy(transaction, binary, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(hash)
+	transactionReceipt, err := connection.Eth.GetTransactionReceipt(hash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(transactionReceipt.ContractAddress)
+	return transactionReceipt.ContractAddress, err
+}
+
+func main() {
+
+	client, err := ethclient.Dial("http://" + GanacheURL)
+	//	key, _ := crypto.GenerateKey()
+	//	auth := bind.NewKeyedTransactor(key)
+	if err != nil {
+		log.Fatal(err)
+	}
+	priv := "2b28b237416b1e48cee1586921077a1faeaef5b8e264f7b49299cc5e697446c8"
+	key, err := crypto.HexToECDSA(priv)
+	contractAddress, err := DeployContract()
+	if err != nil {
+		log.Fatal(err)
+	}
+	auth := bind.NewKeyedTransactor(key)
+	var nonce int64 = 0
+	auth.Nonce = big.NewInt(nonce)
+	fmt.Println(contractAddress)
+	contractAddressHex := common.HexToAddress(contractAddress[2:])
+	storingClient, err := NewStoringDetails(contractAddressHex, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(contractAddressHex, err)
+	//	tx, err := storingClient.SetJWT(auth, []byte("test"))
+	//	data, err := storingClient.GetJWT(nil)
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
+	//	fmt.Printf("Pending TX: 0x%x\n", tx.Hash())
+	//	fmt.Println(string(data))
+	fmt.Println(auth, key, contractAddressHex, storingClient)
+}
