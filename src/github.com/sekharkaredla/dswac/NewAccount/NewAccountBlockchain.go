71be79a8e539465047757df121c23b05475f2162
pranithkumar <praneeth970@gmail.com>
2019-03-02 11:25:42 +0530

    Create a new account in blockchain, still figure out pri and pub key

---
+++ src/github.com/sekharkaredla/dswac/NewAccount/NewAccountBlockchain.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	web3 "github.com/regcostajr/go-web3"
+	providers "github.com/regcostajr/go-web3/providers"
+)
+
+const GanacheURL = "127.0.0.1:7545"
+
+func CreateNewUserInBlockchain() {
+	var connection = web3.NewWeb3(providers.NewHTTPProvider(GanacheURL, 10, false))
+	a, b := connection.Personal.NewAccount("abcd")
+	fmt.Println(a, b)
+	strings, _ := connection.Eth.ListAccounts()
+	fmt.Println(strings)
+}
+
+func main() {
+	CreateNewUserInBlockchain()
+}
